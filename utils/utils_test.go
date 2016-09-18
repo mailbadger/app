7c16d983cae420bfa903c2bf22ffb108a49bceac
Filip Nikolovski <filip.nikol92@gmail.com>
2016-09-18 19:40:31 +0200

    added utils test

---
+++ utils/utils_test.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"encoding/base64"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGenerateRandomBytes(t *testing.T) {
+	b, err := GenerateRandomBytes(16)
+	if err != nil {
+		t.Error("could not generate random bytes: ", err)
+	}
+
+	assert.Len(t, b, 16, "they should be equal")
+}
+
+func TestGenerateRandomString(t *testing.T) {
+	str, err := GenerateRandomString(16)
+	if err != nil {
+		t.Error("could not generate random string: ", err)
+	}
+
+	decoded, err := base64.URLEncoding.DecodeString(str)
+	if err != nil {
+		t.Error("could not decode random string from base64: ", err)
+	}
+
+	assert.Len(t, decoded, 16, "they should be equal")
+}
