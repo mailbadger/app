99332c1c7685ef16e4f24f0378ea811b6379362b
Filip Nikolovski <filip.nikol92@gmail.com>
2016-09-19 09:51:51 +0200

    added vendoring

--- entities/campaign.go
+++ entities/campaign.go
@@ -1,9 +1,17 @@
 package entities
 
+import "time"
+
 //Campaign represents the campaign entity
 type Campaign struct {
-	Id         int64  `json:"id"`
-	UserId     int64  `json:"-"`
-	Name       string `json:"name" sql:"not null"`
-	TemplateId int64  `json:"-"`
+	Id          int64     `json:"id"`
+	UserId      int64     `json:"-"`
+	Name        string    `json:"name" sql:"not null"`
+	Subject     string    `json:"subject"`
+	TemplateId  int64     `json:"-"`
+	Template    Template  `json:"template"`
+	Status      string    `json:"status"`
+	CreatedAt   time.Time `json:"created_at"`
+	ScheduledAt time.Time `json:"scheduled_at"`
+	CompletedAt time.Time `json:"completed_at"`
 }
