14a9701b399b17cea111a13176daa3691a1520fa
Filip Nikolovski <filip.nikol92@gmail.com>
2019-03-23 23:00:32 +0100

    removed unnecessary file

--- emails/sender.go
+++ emails/sender.go
@@ -1,6 +1,9 @@
 package emails
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ses"
 )
 
@@ -11,4 +14,17 @@ type Sender interface {
 
 func NewSesSender(key, secret, region string) (Sender, error) {
 	return NewSESClient(key, secret, region)
+}
+
+func NewSESClient(key, secret, region string) (*ses.SES, error) {
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(region),
+		Credentials: credentials.NewStaticCredentials(key, secret, ""),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ses.New(sess), nil
 }
