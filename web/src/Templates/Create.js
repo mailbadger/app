208f1182cb51fe2b01b1ac502360fe5f92966728
Filip Nikolovski <filip.nikol92@gmail.com>
2019-04-21 18:35:43 +0200

    template validation

--- web/src/Templates/Create.js
+++ web/src/Templates/Create.js
@@ -1,12 +1,32 @@
 import React, { Fragment } from "react";
 import { FormField, Button, TextInput } from "grommet";
 import { Formik, ErrorMessage } from "formik";
+import { string, object } from "yup";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import { UnControlled as CodeMirror } from "react-codemirror2";
+
+const initialHtml = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Html example</title>
+    <meta name="description" content="A simple HTML template" />
+  </head>
+  <body>
+    <h1>Hello {{name}}</h1>
+    <p>Your favorite animal is {{favoriteanimal}}.</p>
+  </body>
+</html>`;
+
+const templateValidation = object().shape({
+  name: string().required("Please enter a template name."),
+  subject: string().required("Please enter a subject for the email."),
+  htmlPart: string().required("Please enter a valid HTML")
+});
 
 const Form = ({
   handleSubmit,
@@ -21,27 +41,37 @@ const Form = ({
 
       <form onSubmit={handleSubmit}>
         <FormField label="Template Name" htmlFor="name">
-          <TextInput name="name" onChange={handleChange} />
+          <TextInput
+            name="name"
+            onChange={handleChange}
+            placeholder="MyTemplate"
+          />
           <ErrorMessage name="name" />
         </FormField>
         <FormField label="Subject" htmlFor="subject">
-          <TextInput name="subject" onChange={handleChange} />
+          <TextInput
+            name="subject"
+            onChange={handleChange}
+            placeholder="Greetings, {{name}}"
+          />
           <ErrorMessage name="subject" />
         </FormField>
 
-        <CodeMirror
-          value={values.html_part}
-          options={{
-            mode: "xml",
-            theme: "material",
-            lineNumbers: true
-          }}
-          onChange={editor => {
-            console.log(editor.getValue());
-            setFieldValue("html_part", editor.getValue(), true);
-          }}
-        />
-
+        <FormField label="HTML Template" htmlFor="htmlPart">
+          <CodeMirror
+            value={values.htmlPart}
+            options={{
+              mode: "xml",
+              theme: "material",
+              lineNumbers: true
+            }}
+            onChange={editor => {
+              console.log(editor.getValue());
+              setFieldValue("htmlPart", editor.getValue(), true);
+            }}
+          />
+          <ErrorMessage name="htmlPart" />
+        </FormField>
         <Button type="submit" primary label="Submit" />
       </form>
     </Fragment>
@@ -52,7 +82,10 @@ const CreateTemplateForm = () => {
   return (
     <Formik
       onSubmit={values => console.log(values)}
-      // validationSchema={loginValidation}
+      validationSchema={templateValidation}
+      initialValues={{
+        htmlPart: initialHtml
+      }}
       render={Form}
     />
   );
