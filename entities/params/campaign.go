478be1d31d5c8908d966cd2941bc262053fc2d15
Darko Djalevski <darko.djalevski@inplayer.com>
2020-09-26 23:43:45 +0200

    1file

--- entities/params/campaign.go
+++ entities/params/campaign.go
@@ -12,3 +12,15 @@ func (p *Campaign) TrimSpaces() {
 	p.Name = strings.TrimSpace(p.Name)
 	p.TemplateName = strings.TrimSpace(p.TemplateName)
 }
+
+// SendCampaign represents request body for POST /api/campaigns/id/start
+type SendCampaign struct {
+	Ids                 []int64           `form:"segment_id[]" validate:"gt=0,dive,required"`
+	Source              string            `form:"source" validate:"required,email,max=191"`
+	FromName            string            `form:"from_name" validate:"required,max=191"`
+	DefaultTemplateData map[string]string `form:"default_template_data" validate:"dive,required"`
+}
+
+func (p *SendCampaign) TrimSpaces() {
+	p.FromName = strings.TrimSpace(p.FromName)
+}
