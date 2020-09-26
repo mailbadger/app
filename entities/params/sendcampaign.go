65ecd97d609c9ae7af68d6c95532c62e8f9252f6
Darko Djalevski <darko.djalevski@inplayer.com>
2020-09-26 17:09:20 +0200

    added validation for slice elements

--- entities/params/sendcampaign.go
+++ entities/params/sendcampaign.go
@@ -3,7 +3,7 @@ package params
 import "strings"
 
 type SendCampaignParams struct {
-	Ids      []int64 `form:"segment_id[]" validate:"required"`
+	Ids      []int64 `form:"segment_id[]" validate:"gt=0,dive,required"`
 	Source   string  `form:"source" validate:"required,email,max=191"`
 	FromName string  `form:"from_name" validate:"required,max=191"`
 }
