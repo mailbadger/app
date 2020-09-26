f2220d5db322f686abb3a9fec9a39a733d1753d2
Darko Djalevski <darko.djalevski@inplayer.com>
2020-09-26 17:25:17 +0200

    rename struct

--- entities/params/sendcampaign.go
+++ entities/params/sendcampaign.go
@@ -2,13 +2,13 @@ package params
 
 import "strings"
 
-// CampaignParams represents request body for POST /api/campaigns/id/start
-type SendCampaignParams struct {
+// SendCampaign represents request body for POST /api/campaigns/id/start
+type SendCampaign struct {
 	Ids      []int64 `form:"segment_id[]" validate:"gt=0,dive,required"`
 	Source   string  `form:"source" validate:"required,email,max=191"`
 	FromName string  `form:"from_name" validate:"required,max=191"`
 }
 
-func (p *SendCampaignParams) TrimSpaces() {
+func (p *SendCampaign) TrimSpaces() {
 	p.FromName = strings.TrimSpace(p.FromName)
 }
