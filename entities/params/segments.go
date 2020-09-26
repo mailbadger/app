060066648c5078415d3197d127b35e13cf3ef77d
Darko Djalevski <darko.djalevski@inplayer.com>
2020-09-26 23:40:58 +0200

    1file

--- entities/params/segments.go
+++ entities/params/segments.go
@@ -12,3 +12,12 @@ type Segment struct {
 func (p *Segment) TrimSpaces() {
 	p.Name = strings.TrimSpace(p.Name)
 }
+
+// SegmentSubs represents request body for PUT /api/segments/{id}/subscribers
+type SegmentSubs struct {
+	Ids []int64 `form:"ids[]" validate:"gt=0,dive,required"`
+}
+
+func (p *SegmentSubs) TrimSpaces() {
+	// no op
+}
