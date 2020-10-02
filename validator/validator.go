6e0fd0c81bef611853ae548f8b2188508534caeb
Darko Djalevski <darko.djalevski@inplayer.com>
2020-10-02 10:04:08 +0200

    cleanup old validation

--- validator/validator.go
+++ validator/validator.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 
 	"github.com/mailbadger/app/entities/params"
@@ -63,48 +62,4 @@ func initValidator() {
 		// if register validation fails panic
 		panic(err)
 	}
-}
-
-type DefaultValidator struct {
-	once     sync.Once
-	validate *validator.Validate
-}
-
-var _ binding.StructValidator = &DefaultValidator{}
-
-func (v *DefaultValidator) ValidateStruct(obj interface{}) error {
-	if kindOfData(obj) == reflect.Struct {
-		v.lazyinit()
-		if err := v.validate.Struct(obj); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func (v *DefaultValidator) Engine() interface{} {
-	v.lazyinit()
-	return v.validate
-}
-
-func (v *DefaultValidator) lazyinit() {
-	v.once.Do(func() {
-		v.validate = validator.New()
-		v.validate.SetTagName("binding") // Print JSON name on validator.FieldError.Field()
-		v.validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
-			name := strings.SplitN(fld.Tag.Get("form"), ",", 2)[0]
-			return name
-		})
-	})
-}
-
-func kindOfData(data interface{}) reflect.Kind {
-	value := reflect.ValueOf(data)
-	valueType := value.Kind()
-
-	if valueType == reflect.Ptr {
-		valueType = value.Elem().Kind()
-	}
-	return valueType
 }
