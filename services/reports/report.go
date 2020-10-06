c471f4f117e713bbc2b8cac5ad7c0cdd6a4ed741
gligor <gligor.gudjevski@inplayer.com>
2020-10-06 22:36:24 +0200

    Add comments

--- services/reports/report.go
+++ services/reports/report.go
@@ -8,24 +8,27 @@ import (
 	"github.com/mailbadger/app/storage"
 )
 
+// ReportService represents all report functionalities
 type ReportService interface {
 	IsAnotherReportRunning(*gin.Context, int64) bool
-	IsLimitExceeded() bool
-	GenerateReport() (*entities.Report, error)
-	UpdateReport() (*entities.Report, error)
-	GenerateCSV()
+	IsLimitExceeded(*gin.Context, int64) bool
+	GenerateReport(*gin.Context) (*entities.Report, error)
+	UpdateReport(*gin.Context) (*entities.Report, error)
+	GenerateCSV(*gin.Context)
 }
 
 type reportService struct {
 	exporter exporters.Exporter
 }
 
+// NewReportService represents constructor for ReportService
 func NewReportService(exporter exporters.Exporter) ReportService {
 	return &reportService{
 		exporter: exporter,
 	}
 }
 
+// IsAnotherReportRunning returns true if there is report in progress for a user or false if all are done
 func (r reportService) IsAnotherReportRunning(c *gin.Context, userID int64) bool {
 	_, err := storage.GetRunningReportForUser(c, userID)
 	if err != nil {
@@ -34,18 +37,18 @@ func (r reportService) IsAnotherReportRunning(c *gin.Context, userID int64) bool
 	return false
 }
 
-func (r reportService) IsLimitExceeded() bool {
+func (r reportService) IsLimitExceeded(c *gin.Context, userID int64) bool {
 	panic("implement me")
 }
 
-func (r reportService) GenerateReport() (*entities.Report, error) {
+func (r reportService) GenerateReport(c *gin.Context) (*entities.Report, error) {
 	panic("implement me")
 }
 
-func (r reportService) UpdateReport() (*entities.Report, error) {
+func (r reportService) UpdateReport(c *gin.Context) (*entities.Report, error) {
 	panic("implement me")
 }
 
-func (r reportService) GenerateCSV() {
+func (r reportService) GenerateCSV(c *gin.Context) {
 	panic("implement me")
 }
