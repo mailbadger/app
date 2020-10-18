fab0e3409fd405bb2b0b07a1b3df0a8b9ae95fc0
Darko Djalevski <darko.djalevski@inplayer.com>
2020-10-18 18:28:30 +0200

    segment actions tests

---
+++ actions/segments_test.go
@@ -0,0 +1,53 @@
+package actions_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/mailbadger/app/entities/params"
+	"github.com/mailbadger/app/storage"
+)
+
+func TestSegments(t *testing.T) {
+	s := storage.New("sqlite3", ":memory:")
+
+	e := setup(t, s)
+	auth, err := createAuthenticatedExpect(e, s)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	e.POST("/api/segments").WithForm(params.Segment{Name: "djale"}).
+		Expect().
+		Status(http.StatusUnauthorized)
+
+	auth.POST("/api/segments").WithForm(params.Segment{Name: ""}).
+		Expect().
+		Status(http.StatusBadRequest).JSON().Object().
+		ValueEqual("message", "Invalid parameters, please try again").
+		ValueEqual("errors", map[string]string{"name": "This field is required"})
+
+	// test post segment
+	auth.POST("/api/segments").WithForm(params.Segment{Name: "djale"}).
+		Expect().
+		Status(http.StatusCreated)
+
+	// test inserted segment
+	auth.GET("/api/segments/1").
+		Expect().
+		Status(http.StatusOK).JSON().Object().
+		ValueEqual("name", "djale").
+		ValueEqual("subscribers_in_segment", 0).
+		ValueEqual("total_subscribers", 0)
+
+	// test put segments
+	auth.PUT("/api/segments/1").WithForm(params.Segment{Name: "djaleputtest"}).
+		Expect().
+		Status(http.StatusNoContent)
+
+	// delete campaign by id
+	auth.DELETE("/api/segments/1").
+		Expect().
+		Status(http.StatusNoContent)
+}
