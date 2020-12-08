ac4800041bd02f25ef2c76b52855b288b6ebefb1
Gligor23 <gligor.gudjevski@inplayer.com>
2020-12-08 11:55:38 +0100

    Add user id in templates table

--- storage/migrations/sqlite3/4_templates.sql
+++ storage/migrations/sqlite3/4_templates.sql
@@ -3,6 +3,7 @@
 CREATE TABLE IF NOT EXISTS "templates"
 (
     "id"         integer primary key autoincrement,
+    `user_id`    integer unsigned NOT NULL,
     `name`       varchar(191) NOT NULL,
     "subject"    varchar(191) NOT NULL,
     `text_part`  text,
