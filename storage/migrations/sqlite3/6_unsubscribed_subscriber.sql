7a3da9b7448d17ac5f8b09dfec892200c4b60bf8
Darko Djalevski <darko.djalevski@inplayer.com>
2021-02-18 17:41:06 +0100

    fix ai on sqllite

--- storage/migrations/sqlite3/6_unsubscribed_subscriber.sql
+++ storage/migrations/sqlite3/6_unsubscribed_subscriber.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE IF NOT EXISTS `unsubscribed_subscriber`
 (
-    `id`         integer unsigned primary key  auto_increment NOT NULL,
-    `email`      varchar(191)              NOT NULL,
-    `created_at` datetime(6)               NOT NULL
+    `id`         integer unsigned primary key NOT NULL,
+    `email`      varchar(191)                 NOT NULL,
+    `created_at` datetime(6)                  NOT NULL
 );
 
 -- +migrate Down
