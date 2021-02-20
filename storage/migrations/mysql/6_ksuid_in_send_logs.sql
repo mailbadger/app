b379a55aa7b67903c58e1d3fdb67fd0bdaa5bd92
gligor <gligor.gudjevski@inplayer.com>
2021-02-20 22:32:11 +0100

    Remove alter table sql files

--- storage/migrations/mysql/6_ksuid_in_send_logs.sql
+++
@@ -1,9 +0,0 @@
--- +migrate Up
-
-ALTER TABLE `send_logs` MODIFY `id` varbinary(27);
-ALTER TABLE `send_logs` DROP COLUMN `uuid`;
-
--- +migrate Down
-
-ALTER TABLE `send_logs` MODIFY `id` bigint unsigned;
-ALTER TABLE `send_logs` ADD COLUMN `uuid` varchar(36) unique NOT NULL AFTER `id`;
