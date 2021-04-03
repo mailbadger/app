fe45014ab0f95977fb06aecfd3f527cc1b46c8ec
Darko Djalevski <dare_zalevski@yahoo.com>
2021-04-03 02:02:36 +0200

    migration and entity for scheduled campaigns

---
+++ entities/scheduledcampaigns.go
@@ -0,0 +1,15 @@
+package entities
+
+import (
+	"time"
+
+	"github.com/segmentio/ksuid"
+)
+
+type ScheduledCampaigns struct {
+	ID          ksuid.KSUID `json:"id" gorm:"column:id; primary_key:yes"`
+	CampaignID  int64       `json:"campaign_id"`
+	ScheduledAt time.Time   `json:"scheduled_at"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
+}
