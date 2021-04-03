75bfff8dfde1c3096555147088a4019e454fdf69
Darko Djalevski <dare_zalevski@yahoo.com>
2021-04-03 02:10:22 +0200

    fix migrations|

--- storage/migrations/mysql/5_scheduled_campaigns.sql
+++ storage/migrations/mysql/5_scheduled_campaigns.sql
@@ -5,11 +5,10 @@ CREATE TABLE IF NOT EXISTS `scheduled_campaigns`
     `id`            varbinary(27)       primary key,
     `campaign_id`   integer unsigned    NOT NULL,
     `scheduled_at`  datetime(6)         NOT NULL,
-    `description`   varchar(191)        NOT NULL,
     `created_at`    datetime(6),
     `updated_at`    datetime(6),
     FOREIGN KEY (`campaign_id`) REFERENCES campaigns (`id`)
-    ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    );
 
 -- +migrate Down
 
