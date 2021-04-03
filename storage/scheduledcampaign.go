f70db2560db83a5ffa35edf6d964b40770b2f043
Darko Djalevski <dare_zalevski@yahoo.com>
2021-04-04 00:38:19 +0200

    dep fix'

--- storage/scheduledcampaign.go
+++ storage/scheduledcampaign.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 
 	"github.com/jinzhu/gorm"
-	"github.com/segmentio/ksuid"
 
 	"github.com/mailbadger/app/entities"
 )
@@ -22,6 +21,6 @@ func (db *store) CreateScheduledCampaign(c *entities.ScheduledCampaign) error {
 }
 
 // DeleteScheduledCampaign deletes a scheduled campaign.
-func (db *store) DeleteScheduledCampaign(id ksuid.KSUID, campaignID int64) error {
-	return db.Where("campaign_id = ?", campaignID).Delete(entities.ScheduledCampaign{ID: id}).Error
+func (db *store) DeleteScheduledCampaign(campaignID int64) error {
+	return db.Where("campaign_id = ?", campaignID).Delete(entities.ScheduledCampaign{}).Error
 }
