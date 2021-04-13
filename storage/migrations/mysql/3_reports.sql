8369e896cfac7c649de1bdfa5ca15260e8dffe1e
gligor <gligor.gudjevski@inplayer.com>
2021-04-13 23:31:46 +0200

    Formated and sync all migrations

--- storage/migrations/mysql/3_reports.sql
+++ storage/migrations/mysql/3_reports.sql
@@ -1,7 +1,6 @@
 -- +migrate Up
 
-CREATE TABLE IF NOT EXISTS `reports`
-(
+CREATE TABLE IF NOT EXISTS `reports` (
     `id`         integer unsigned primary key AUTO_INCREMENT NOT NULL,
     `user_id`    integer unsigned                            NOT NULL,
     `resource`   varchar(191)                                NOT NULL,
@@ -12,7 +11,8 @@ CREATE TABLE IF NOT EXISTS `reports`
     `created_at` datetime(6)                                 NOT NULL,
     `updated_at` datetime(6)                                 NOT NULL,
     FOREIGN KEY (`user_id`) REFERENCES users (`id`),
-    INDEX user_id_resource (`user_id`, `resource`, `file_name`, `type`)
+    INDEX idx_user_id (`user_id`),
+    INDEX idx_user_id_resource (`user_id`, `resource`, `file_name`, `type`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- +migrate Down
--- storage/migrations/sqlite3/2_tokens.sql
+++ storage/migrations/sqlite3/2_tokens.sql
@@ -1,14 +1,14 @@
 -- +migrate Up
 
 CREATE TABLE IF NOT EXISTS "tokens" (
-  "id"         integer primary key autoincrement,
-  "user_id"    integer,
-  "token"      varchar(191) NOT NULL UNIQUE,
-  "type"       varchar(191) NOT NULL,
-  "expires_at" datetime NOT NULL,
-  "created_at" datetime NOT NULL,
-  "updated_at" datetime NOT NULL,
-  foreign key ("user_id") references users("id")
+    "id"         integer primary key autoincrement,
+    "user_id"    integer,
+    "token"      varchar(191) NOT NULL UNIQUE,
+    "type"       varchar(191) NOT NULL,
+    "expires_at" datetime NOT NULL,
+    "created_at" datetime NOT NULL,
+    "updated_at" datetime NOT NULL,
+    foreign key ("user_id") references users("id")
 );
 
 -- +migrate Down
