2b5e6359618df0a09f7fb2c63f07d7ab472b9979
Filip Nikolovski <me@filipnikolovski.com>
2021-06-21 21:13:47 +0200

    refactor: moved csrf handler to middleware package

--- routes/routes.go
+++ routes/routes.go
@@ -14,8 +14,6 @@ import (
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/contrib/ginrus"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/csrf"
-	adapter "github.com/gwatts/gin-adapter"
 	"github.com/open-policy-agent/opa/ast"
 	"github.com/sirupsen/logrus"
 	"github.com/unrolled/secure"
@@ -178,31 +176,11 @@ func New() http.Handler {
 		handler,
 		opacompiler,
 		middleware.NoCache(),
-		CSRF(),
+		middleware.CSRF(),
 		tollbooth_gin.LimitHandler(lmt),
 	)
 
 	return handler
-}
-
-// CSRF returns a handler which performs checks for CSRF tokens.
-func CSRF() gin.HandlerFunc {
-	secureCookie, _ := strconv.ParseBool(os.Getenv("SECURE_COOKIE"))
-	csrfMd := csrf.Protect([]byte(os.Getenv("SESSION_AUTH_KEY")),
-		csrf.MaxAge(0),
-		csrf.Secure(secureCookie),
-		csrf.Path("/api"),
-		csrf.SameSite(csrf.SameSiteStrictMode),
-		csrf.ErrorHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusForbidden)
-			_, err := w.Write([]byte(`{"message": "Forbidden - CSRF token invalid"}`))
-			if err != nil {
-				logrus.Error(err)
-			}
-		})),
-	)
-
-	return adapter.Wrap(csrfMd)
 }
 
 // SetGuestRoutes sets the guest routes to the gin engine handler along with
