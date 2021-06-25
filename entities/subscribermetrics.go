bbfb660e31ea52a89c6cb63805c1dc50c28f4ab3
gligor <gligor.gudjevski@inplayer.com>
2021-06-25 18:18:31 +0200

    Update migration

--- entities/subscribermetrics.go
+++ entities/subscribermetrics.go
@@ -6,10 +6,10 @@ import (
 
 // SubscribersMetrics represents daily events per user
 type SubscribersMetrics struct {
-	ID           int64 `json:"id" gorm:"column:id; primary_key:yes"`
-	UserID       int64       `json:"user_id"`
-	Created      int64       `json:"created"`
-	Deleted      int64       `json:"deleted"`
-	Unsubscribed int64       `json:"unsubscribed"`
-	Date         time.Time   `json:"date"`
+	ID           int64     `json:"id" gorm:"column:id"`
+	UserID       int64     `json:"user_id" gorm:"primaryKey"`
+	Created      int64     `json:"created"`
+	Deleted      int64     `json:"deleted"`
+	Unsubscribed int64     `json:"unsubscribed"`
+	Date         time.Time `json:"date" gorm:"primaryKey"`
 }
--- storage/migrations/mysql/6_subscriber_metrics.sql
+++ storage/migrations/mysql/6_subscriber_metrics.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS `jobs` (
     `id`                INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     `name`              VARCHAR(30) UNIQUE NOT NULL,
     `last_processed_id` BIGINT UNSIGNED NOT NULL,
-    `created_at`        datetime(6)            NOT NULL,
-    `updated_at`        datetime(6)            NOT NULL,
+    `created_at`        datetime(6) NOT NULL,
+    `updated_at`        datetime(6) NOT NULL,
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 INSERT INTO `jobs` (
@@ -16,13 +16,13 @@ INSERT INTO `jobs` (
 ) VALUES ("subscriber_metrics", 0, NOW(), NOW())
 
 CREATE TABLE IF NOT EXISTS `subscriber_metrics` (
-    `id`                BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `id`                BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
     `user_id`           INTEGER UNSIGNED NOT NULL,
-    `total_subscribers` INTEGER UNSIGNED NOT NULL,
     `created`           INTEGER UNSIGNED NOT NULL,
     `unsubscribed`      INTEGER UNSIGNED NOT NULL,
     `deleted`           INTEGER UNSIGNED NOT NULL,
-    `date`              date         NOT NULL,
+    `date`              date NOT NULL,
+    PRIMARY KEY (`user_id`, `date`),
     FOREIGN KEY (`user_id`) REFERENCES users (`id`)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
